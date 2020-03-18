c8d10e61f3e2daf1c05de3e84de9c1f1b5a69159
Alistair Chapman <alistair@agchapman.com>
2020-03-18 14:08:21 +1000

    Initial commit

---
+++ src/SysInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
